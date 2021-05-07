c11594d44bb0959c36815f2b5c1618bc05bfa999
nitinsaini1008 <nitinsaini1008@gmail.com>
2021-05-07 17:44:37 +0530

    new_1

--- l1/models.py
+++ l1/models.py
@@ -8,16 +8,7 @@ class answere(models.Model):
 		C = "C", _("C")
 		D = "D", _("D")
 	ans=models.CharField(max_length=1,choices=options.choices)
-	question=models.ForeignKey(scq,on_delete=models.CASCADE)
-
-class scq(models.Model):
-	user=models.ForeignKey(User,on_delete=models.CASCADE)
-	question=models.CharField(max_length=200)
-	A=models.CharField(max_length=100)
-	B=models.CharField(max_length=100)
-	C=models.CharField(max_length=100)
-	D=models.CharField(max_length=100)
-	ans=models.ForeignKey(answere,on_delete=models.CASCADE)
+	question=models.ForeignKey(mcq,on_delete=models.CASCADE)
 
 class mcq(models.Model):
 	user=models.ForeignKey(User,on_delete=models.CASCADE)
@@ -26,22 +17,17 @@ class mcq(models.Model):
 	B=models.CharField(max_length=100)
 	C=models.CharField(max_length=100)
 	D=models.CharField(max_length=100)
-	ans=models.ManyToManyField(answere)
+
 
 class true_false(models.Model):	
 	user=models.ForeignKey(User,on_delete=models.CASCADE)
 	question=models.CharField(max_length=200)
 	ans=models.BooleanField(default=False)
 
-class ans_given_scq(models.Model):
-	user=models.ForeignKey(User,on_delete=models.CASCADE)
-	question=models.ForeignKey(scq,on_delete=models.CASCADE)
-	ans=models.ForeignKey(answere,on_delete=models.CASCADE)
 
 class ans_given_mcq(models.Model):
 	user=models.ForeignKey(User,on_delete=models.CASCADE)
-	question=models.ForeignKey(mcq,on_delete=models.CASCADE)
-	ans=models.ManyToManyField(answere)
+	ans=models.ForeignKey(answere,on_delete=models.CASCADE)
 
 class ans_given_bool(models.Model):
 	user=models.ForeignKey(User,on_delete=models.CASCADE)
